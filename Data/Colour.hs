0571129be9b6ad00627742248cb38edf40d99088
Russell O'Connor <darcs@roconnor.internationalstolengoods.org>
2008-10-19 18:28:25 +0200

    Show instance for Colour.
    darcs-hash:20081019162825-130f7-0934fd11c0f0aa3a35018c39c595c21634c4885d

--- Data/Colour.hs
+++ Data/Colour.hs
@@ -59,6 +59,13 @@ colourConvert :: (Fractional b, Real a) => Colour a -> Colour b
 colourConvert (RGB r g b) =
   RGB (Chan.convert r) (Chan.convert g) (Chan.convert b)
 
+instance (Real a) => Show (Colour a) where
+  showsPrec _ c = ("(rgb709 "++) . (shows r) . (" "++)
+                                 . (shows b) . (" "++)
+                                 . (shows b) . (")"++)
+   where
+    (r,g,b) = toRGB709 c
+
 data Alpha = Alpha
 -- premultiplied alpha
 data AlphaColour a = RGBA !(Colour a) !(Chan Alpha a) deriving (Eq)
